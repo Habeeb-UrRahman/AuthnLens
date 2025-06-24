c78d1246d3979e0885004225c684c83139cf2a74
Habeeb Ur Rahman <rahman.habeeb@outlook.com>
2025-06-24 17:53:29 +0530

    Update ImageDetection.tsx

--- src/pages/ImageDetection.tsx
+++ src/pages/ImageDetection.tsx
@@ -11,7 +11,8 @@ import { Loader } from 'lucide-react';
 
 // Define API endpoint
 // const API_URL = 'http://localhost:5000';
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
+// const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 
 const ImageDetection = () => {
