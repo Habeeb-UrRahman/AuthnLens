9268fee802ca5e07f942f2d39f76bd0085b79cdc
Habeeb Ur Rahman <rahman.habeeb@outlook.com>
2025-06-24 18:44:49 +0530

    Update VideoDetection.tsx

--- src/pages/VideoDetection.tsx
+++ src/pages/VideoDetection.tsx
@@ -11,7 +11,8 @@ import { Loader } from 'lucide-react';
 
 // Define API endpoint
 // const API_URL = 'http://localhost:5000';
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
+// const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
+const API_URL = "https://authnlens-backend-674458572550.asia-south1.run.app/";
 
 
 const VideoDetection = () => {
